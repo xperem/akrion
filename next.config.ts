3b62ab0cb047b55c0d664ba8864b0fa5e1f49487
Maxence Pereira <maxencepereira@macbook-pro.home>
2025-07-13 23:58:19 +0200

    chore: ignore ESLint errors during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Ignore ESLint errors during production builds (utile pour Vercel)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
